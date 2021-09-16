df4aa2e2c2abef9ecba48c8b7355c1a7e21d0fec
Gabriel <gabrielcemp@gmail.com>
2021-09-16 20:16:46 -0300

    feat: add custom error class

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,9 +1,26 @@
 import 'dotenv/config'
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import '../mongoose'
+import { AppError } from '../../errors/AppError'
 
 const app = express()
 
 app.use(express.json())
 
 export { app }
+
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message
+    })
+  }
+
+  console.error(err)
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error'
+  })
+})
